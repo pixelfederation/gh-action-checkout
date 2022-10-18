672b20080279b742bbb12203b95a0e70c83ccc5c
tombokombo <tombo@sysart.tech>
2022-10-18 15:53:18 +0200

    don't checkout if not needed

--- src/checkout/index.ts
+++ src/checkout/index.ts
@@ -13,9 +13,9 @@ export async function checkoutRepos(): Promise<void> {
         }
         const [ repo, ref, dst, depth, token ] = repository.split("|").map(item => item.trim());
         let dirExist: boolean = false;
+        let dstPath = dst.startsWith('/') ? dst : `${ci.dir}/${dst}/`;
+        dstPath = dstPath.endsWith('/') ? dstPath : `${dstPath}/`;
         try {
-            let dstPath = dst.startsWith('/') ? dst : `${ci.dir}/${dst}/`;
-            dstPath = dstPath.endsWith('/') ? dstPath : `${dstPath}/`;
             let data = await fs.promises.readFile(`${dstPath}/.git/config`, { encoding: "utf8"});
             if(token && typeof token !== "undefined") {
                 data = data.replace(new RegExp("url\\s?=\\s?https://(.+@)?github.com", 'g'), `url = https://${token}@github.com`);
@@ -30,8 +30,18 @@ export async function checkoutRepos(): Promise<void> {
         const fullRepoPath = ( token && typeof token !== "undefined" )? `https://${token}@github.com/${repo}` : `https://github.com/${repo}`;
         let gitArgs: string[];
         if(dirExist) {
-            gitArgs = ["cd", dst, ";", "git", "checkout" , ref , ";", "git", "fetch", ";",
-                     "git", "reset", "--hard", `origin/${ref}`, ";", "git", "clean", "-d", "-f"]
+            let HEAD: string = await fs.promises.readFile(`${dstPath}/.git/HEAD`, { encoding: "utf8"});
+            let branch: string = "";
+            try {
+                branch = HEAD.match(new RegExp(".+\\srefs/heads/(.*)",""))[1];
+            } catch(err) {
+                console.log("Fail to read HEAD", err)
+            }
+            gitArgs = ["cd", dst, ";"];
+            if (branch != ref) {
+                gitArgs =  gitArgs.concat(["git", "checkout" , ref, ";"]);
+            }
+            gitArgs = gitArgs.concat([ "git", "fetch", ";", "git", "reset", "--hard", `origin/${ref}`, ";", "git", "clean", "-d", "-f"]);
         } else {
             gitArgs = ["git", "clone", fullRepoPath, "-b", ref];
             if (depth && typeof depth !== "undefined") {
